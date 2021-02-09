47efc5c4d61a89832e0c55c5eb0572e1b222fe1d
Fatymata <fatymata.gassama@gmail.com>
2021-02-09 15:53:34 +0100

    final layout done

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page>
+  <q-page class="q-ma-lg">
     <h2>Liste de produits</h2>
-    <div class="flex">
+    <div class="row q-gutter-xl q-mx-auto">
       <Product
         v-for="(item, idx) in products"
         :key="idx"
@@ -9,6 +9,8 @@
         :description="item.desc"
         :price="item.price"
         :img="item.img"
+        class="col-12 col-md "
+        style="min-width : 25vw; max-width : 80vw"
       />
     </div>
   </q-page>
